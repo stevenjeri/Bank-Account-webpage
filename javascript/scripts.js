53a09b0acc65a706f4540e3bf52a76ce3ccc1de4
stephen njeri <stevenjeri33@gmail.com>
2021-06-24 22:33:37 +0300

    add new file images,.vscode/settings.json, modified css/styles.css, index.html and javascript/script.js

---
+++ javascript/scripts.js
@@ -0,0 +1,45 @@
+//Business logic
+function BankAccount(name, initialDeposit, balance) {
+    this.userName = name;
+    this.firstDeposit = initialDeposit;
+}
+
+// adding a deposit
+BankAccount.prototype.makeDeposit = function(deposit) {
+    this.balance += deposit;
+}
+
+//subtracting a withdrawal
+BankAccount.prototype.makeWithdrawal = function(withdrawal) {
+    this.balance -= withdrawal;
+}
+
+//User logic
+$(function() {
+    $('#submit-new-account').submit(function(event) {
+        event.preventDefault();
+        let name = $('#user-name').val();
+        let initialDeposit = parseInt($('#initial-deposit').val());
+        let newBalance = initialDeposit;
+        const userAccount = new BankAccount(name, initialDeposit, newBalance);
+        console.log(userAccount);
+        $('.new-account').remove();
+        $('.balance-here').text(`$${userAccount.balance}`);
+
+        $('#make-deposit').submit(function(event) {
+            event.preventDefault();
+            let deposit = parseInt($('#current-deposit').val());
+            userAccount.makeDeposit(deposit);
+            $('.balance-here').text(`$${userAccount.balance}`);
+            $('#make-deposit').trigger('reset');
+        });
+
+        $('#make-withdrawal').submit(function(event) {
+            event.preventDefault();
+            let withdrawal = parseInt($('#current-withdrawal').val());
+            userAccount.makeWithdrawal(withdrawal);
+            $('.balance-here').text(`$${userAccount.balance}`);
+            $('#make-withdrawl').trigger('reset');
+        });
+    });
+});
